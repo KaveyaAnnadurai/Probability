13194911b99e03c8420da436578e6836b5b9923d
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-31 14:50:57 +0530

    Req1: Add. Included logic and test cases for checking equality in probability.

--- src/main/java/com/m2p/ProbabilityChecker.java
+++ src/main/java/com/m2p/ProbabilityChecker.java
@@ -1,4 +1,29 @@
 package com.m2p;
 
 public class ProbabilityChecker {
+
+        private final double probabilityValue;
+
+        public ProbabilityChecker(double probabilityValue){
+            this.probabilityValue = probabilityValue;
+        }
+
+        @Override
+        public boolean equals(Object probabilityObject){
+                if(this == probabilityObject){
+                        return true;
+                }
+                if(probabilityObject == null || probabilityObject.getClass() != getClass()){
+                     return false;
+                }
+                ProbabilityChecker that = (ProbabilityChecker) probabilityObject;
+
+                if(that.probabilityValue == probabilityValue)
+                        return true;
+                return false;
+        }
+
+
+
+
 }
--- src/test/java/com/m2p/ProbabilityCheckerTest.java
+++ src/test/java/com/m2p/ProbabilityCheckerTest.java
@@ -1,5 +1,29 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
 public class ProbabilityCheckerTest {
-    ProbabilityChecker probabilityChecker = new ProbabilityChecker();
+    @Test
+    void toCheckProbabilityOfHeadsEqualToProbabilityOfTails(){
+       ProbabilityChecker probabilityOfHeads = new ProbabilityChecker(0.5);
+       ProbabilityChecker probabilityOfTails = new ProbabilityChecker(0.5);
+
+       assertThat(probabilityOfHeads, is(equalTo(probabilityOfTails)));
+    }
+    @Test
+    void toCheckInEqualiyProbabilityOfHeadsEqualToProbabilityOfTails(){
+        ProbabilityChecker probabilityOfHeads = new ProbabilityChecker(0.4);
+        ProbabilityChecker probabilityOfTails = new ProbabilityChecker(0.5);
+
+        assertThat(probabilityOfHeads, is(not(equalTo(probabilityOfTails))));
+    }
+
+
+
+
+
 }
